1a32cd129c0e6ee013c652437799c7c4b2fba4e2
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-02 13:09:55 +0530

    Build array using function

---
+++ 34_problem.py
@@ -0,0 +1,15 @@
+# Consider a generator function that generates 10 integers and use it to build an array
+
+import numpy as np
+
+def generate():
+    for i in range(10):
+        yield i
+
+# yield returns one value at a time
+arr = np.fromiter(generate(), dtype=int)
+
+# generate() --> creates the generator
+# np.fromiter() --> reads values from the generator
+
+print(arr)
