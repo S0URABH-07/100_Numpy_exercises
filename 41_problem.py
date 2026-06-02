3d78dde8caa063f5740249b4171d4c91d37b049f
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-02 14:06:39 +0530

    Compare two array equal or not Use array_equal()

---
+++ 41_problem.py
@@ -0,0 +1,12 @@
+# Consider two random arrays A and B, check if they are equal
+import numpy as np
+
+A = np.random.randint(0, 10, 5)
+B = np.random.randint(0, 10, 5)
+
+equal = np.array_equal(A, B)
+# array_equal() --> return the value (True and False)
+
+print("A =", A)
+print("B =", B)
+print("Are they equal?", equal)
