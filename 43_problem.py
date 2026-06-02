f17e247e3e6279854dfd026e640b45785860b505
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-02 14:48:15 +0530

    Cartesian coordinates, Radius , Angle

---
+++ 43_problem.py
@@ -0,0 +1,19 @@
+# Consider a random 10x2 matrix representing cartesian coordinates, convert them to polar coordinates
+
+import numpy as np
+arr = np.random.random((10, 2))
+
+X = arr[:, 0]
+Y = arr[:, 1]
+
+R = np.sqrt(X**2 + Y**2)
+T = np.arctan2(Y, X)
+
+print("Cartesian Coordinates:")
+print(arr)
+
+print("\nRadius (r):")
+print(R)
+
+print("\nAngle (θ):")
+print(T)
