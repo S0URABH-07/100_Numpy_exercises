15cd768ada199ee773b9b26e36039ca7f4e9e678
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-03 13:59:54 +0530

    Find closest max value USE -> .min()

---
+++ 50_problem.py
@@ -0,0 +1,8 @@
+# How to find the closest max value (to a given scalar) in a vector?
+
+import numpy as np
+arr = np.array([10, 20, 30, 40, 50])
+value = 33
+
+closest_max = arr[arr >= value].min()
+print(closest_max)
