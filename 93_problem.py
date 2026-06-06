57ab93681e01bf32827fe2d581c7fbed8847ec00
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-06-06 12:55:51 +0530

    Plot Line Graph from NumPy Array

---
+++ 93_problem.py
@@ -0,0 +1,11 @@
+# Plot Line Graph from NumPy Array
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.array([1,2,3,4,5])
+y = np.array([10,20,15,30,25])
+plt.plot(x, y)
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Line Graph")
+plt.show()
