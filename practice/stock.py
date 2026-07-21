07f4c9c1df1f0a6798564fd5774ed3f7ae99a6cc
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-21 21:18:43 +0530

    Stock Market

---
+++ practice/stock.py
@@ -0,0 +1,24 @@
+import numpy as np
+prices = np.array([
+    120,
+    123,
+    125,
+    122,
+    130,
+    135,
+    132,
+    140
+])
+change = np.diff(prices)
+
+print(change)
+
+print(np.max(change))
+
+print(np.min(change))
+
+print(np.mean(change))
+
+growth = ((prices[-1]-prices[0])/prices[0])*100
+
+print(growth)
