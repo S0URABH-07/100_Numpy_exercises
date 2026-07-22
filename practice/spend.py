fcd52ee4c4b40e2fc2e1b6e091964bead57c14eb
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-22 19:04:17 +0530

    spend

--- practice/spend.py
+++ practice/spend.py
@@ -1,14 +1,5 @@
 import numpy as np
-spending = np.array([
-    1200,
-    2500,
-    900,
-    5000,
-    7000,
-    1500,
-    3500,
-    4500
-])
+spending = np.array([1200,2500,900,5000,7000,1500,3500,4500])
 print(np.mean(spending))
 
 print(spending[spending > 4000])
