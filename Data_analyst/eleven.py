dab993b84bbcf217a83b6587f16e8a3b2b101e34
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-07-23 22:03:41 +0530

    Conditions

---
+++ Data_analyst/eleven.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+var = pd.read_csv("HR_Analytics.csv")
+
+data = var.to_numpy()
+age = data[:,0].astype(int)
+income = data[:,18].astype(int)
+overtime = data[:,22]
+attrition = data[:,1]
+
+condition = (
+    (age > 35) &
+    (income > 12000) &
+    (overtime == "Yes") &
+    (attrition == "Yes")
+)
+
+print(data[condition])
