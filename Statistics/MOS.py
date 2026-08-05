6a711c0fdc40bfa8b4a85fe5ea048927f1fb6d05
Sourabh Dhakad <sourabhdhakad8349@gmail.com>
2026-08-05 21:37:24 +0530

    Central Tendency

---
+++ Statistics/MOS.py
@@ -0,0 +1,11 @@
+# Measure of Spread  (Range, IQR, Outliers, Five Number Summary)
+marks = [65, 78, 82, 90, 55, 72, 88, 95, 68, 80]
+
+maximum = max(marks)
+minimum = min(marks)
+
+range_value = maximum - minimum
+
+print("Maximum:", maximum)
+print("Minimum:", minimum)
+print("Range:", range_value)
